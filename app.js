dc17324b312c945e1c72c8aa11cca8944a6cbd78
Jarred 'Beatnik' Hauschild <contact@beatnik.id.au>
2018-05-16 19:52:12 +0930

    Added config file for user options

--- app.js
+++ app.js
@@ -1,48 +1,91 @@
+var CONFIG = require("./config.json");
 var fs = require("fs");
 var express = require("express");
 var app = express();
-
-// Hardcoded Labels - add the full path to each label you want to include
-//                  - ensure to use forward slash (/) not backslash (\)
-var paths = [
-  'c:/path/to/most_recent_follower.txt'
-  ,'c:/path/to/most_recent_subsicriber.txt'
-  ,'c:/path/to/most_recent_cheer.txt'
-  ,'c:/path/to/most_recent_donation.txt'
-];
 var labels = [];
+var delay = 0;
+var CSStrans = ``;
+var CSSsize = ``;
+var CSSpos = ``;
+var CSSdeco = ``;
+var CSStext = ``;
 
-//Setup express environment variables
+// Setup express environment variables
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// Express Route - generate labels and render page
+// Initialise user configuration options
+function setUserOptions (trans, deco) {
+
+  // transitions
+  if (trans === "swipeUp" || trans === "swipeDown") {
+    CSSsize = `\n  height: 0px\n`;
+    CSStrans = `\n  @keyframes cycleLabels {\n  0%, 100% {height: 0px;}\n  7%, 93% {height: 50px;}\n}`
+    if (trans === "swipeUp") {
+      CSSpos = `\n  text-align: center;`
+    }
+    else if (trans === "swipeDown") {
+      CSSpos = `\n  position: absolute;\n  text-align: center;\n  top: 0;\n  left auto;\n  right: auto;`
+    }
+  }
+
+  else if (trans === "swipeLeft" || trans === "swipeRight") {
+    console.log("Swipe Left and Swipe Right not supported yet")
+  };
+
+  // text decoration
+  if (deco === "glow") {
+    console.log(`text decoration set to ${deco}`);
+    CSSdeco = `\n  text-shadow: 1px 0 ${CONFIG.text.decoration.size}px ${CONFIG.text.decoration.colour}, -1px 0 ${CONFIG.text.decoration.size}px ${CONFIG.text.decoration.colour}, 0 -1px ${CONFIG.text.decoration.size}px ${CONFIG.text.decoration.colour}, 0 1px ${CONFIG.text.decoration.size}px ${CONFIG.text.decoration.colour};`
+  }
+  else if (deco === "outline") {
+    CSSdeco = `\n  -webkit-text-stroke: ${CONFIG.text.decoration.size}px ${CONFIG.text.decoration.colour};`
+  };
+
+  if (CONFIG.text.bold === "yes") {
+    CSStext = CSStext + `\n  font-weight: bold;`
+  };
+
+  if (CONFIG.text.italic === yes) {
+    CSStext = CSStext + `\n  font-style: italic;`
+  };
+};
+
+setUserOptions(CONFIG.transition.type, CONFIG.text.decoration.type, CONFIG.text.wrap);
+
+var style = `<style>\n#text {\n  margin: auto;\n  overflow: hidden;\n  font-family: ${CONFIG.text.font};\n  font-size: ${CONFIG.text.size}px;\n  border-bottom: ${CONFIG.text.size * 0.2}px;\n  color: ${CONFIG.text.colour};${CSStext}${CSSdeco}${CSSpos}${CSSsize}}\n${CSStrans}\n</style>`;
+
+// Express Route - generate divs and render page
 app.get('/', function(req, res) {
+
   labels = [];
-  function setLabels (path, cb) {
+  body = ``;
+  delay = 0;
+
+  function setLabels (file, cb) {
     setTimeout(() => {
-      fs.readFile(path, 'utf8', function(err, data) {
+      fs.readFile(file, 'utf8', function(err, data) {
         if (err) {
-          console.log('error with ' + path + ': ' + err);
+          console.log(`error with ${file}: ${err}`);
         }
-        labels.push(data.toUpperCase());
+        body = body + `    <div id="text" style="animation: cycleLabels ${CONFIG.transition.time}s ${delay}s 1 ease-in-out alternate;">\n      ${data.toUpperCase()}    </div>\n`;
+        delay = delay + CONFIG.transition.time;
       });
       setTimeout(() => {cb()}, 50);
     }, 50);
   };
 
-  let requests = paths.reduce((promiseChain, path) => {
+  let requests = CONFIG.files.reduce((promiseChain, file) => {
       return promiseChain.then(() => new Promise((resolve) => {
-        setLabels(path, resolve);
+        setLabels(file, resolve);
       }));
   }, Promise.resolve());
 
 // Add or remove lines to match the amount of labels you want
   requests.then(() => res.render('carousel', {
-    label1: labels[0],
-    label2: labels[1],
-    label3: labels[2],
-    label4: labels[3]
+    refresh: delay,
+    style: style,
+    body: body
   }));
   requests.then(() => console.log('rendered ' + requests));
 });
